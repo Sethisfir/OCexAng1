2fbb3b399ce311b8a1a8528ce7eeab9b68e91cc0
sethisfir <micka_8@hotmail.fr>
2018-09-20 11:32:45 +0200

    mise a jour

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'blog';
+
+  posts = [
+  	{
+  		title: "First Post",
+  		content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec nec velit ut odio tempus tempus ac a diam. Nulla facilisi.",
+  		loveIts: 1,
+  		created_at: new Date()
+  	},
+  	{
+  		title: "Second Post",
+  		content: "Phasellus efficitur nulla eget sapien tincidunt vehicula. Integer mollis enim varius, volutpat tellus vitae, aliquam felis.",
+  		loveIts: -1,
+  		created_at: new Date()
+  	},
+  	{
+  		title: "Third Post",
+  		content: "Fusce sollicitudin dolor id nulla venenatis, in aliquam nunc condimentum. Cras sit amet elit sit amet velit accumsan consequat.",
+  		loveIts: 0,
+  		created_at: new Date()
+  	}
+  ];
 }
